231aa14715e2e2bffabb6b0c5670e841e59c1f74
Allen Zheng <strburst@users.noreply.github.com>
2015-06-22 22:05:55 -0400

    Add a basic, working BST data type

---
+++ BinTree.hs
@@ -0,0 +1,80 @@
+module BinTree (
+    add,
+    makeTree,
+    addAll,
+    preorder,
+    inorder,
+    postorder,
+    minOf,
+    maxOf,
+    remove
+    ) where
+
+-- A typical binary tree, holding data and left/right branches
+data BinTree a = BinEmpty
+               | BinBranch a (BinTree a) (BinTree a)
+               deriving (Eq, Show)
+
+binTree1 = BinBranch 2
+    (BinBranch 1 BinEmpty BinEmpty)
+    (BinBranch 3 BinEmpty BinEmpty)
+
+binTree2 = BinBranch 10
+    (BinBranch 5
+        (BinBranch 4 BinEmpty BinEmpty)
+        (BinBranch 6 BinEmpty BinEmpty))
+    (BinBranch 15
+        (BinBranch 14 BinEmpty BinEmpty)
+        BinEmpty)
+
+binTree3 = foldl (flip add) BinEmpty [4,1,6,2,5,9]
+
+add :: (Ord a) => a -> BinTree a -> BinTree a
+add item (BinBranch root left right)
+    | item < root = BinBranch root (add item left) right
+    | item > root = BinBranch root left (add item right)
+    | otherwise   = BinBranch root left right
+add item BinEmpty = BinBranch item BinEmpty BinEmpty
+
+makeTree :: (Ord a, Foldable t) => t a -> BinTree a
+makeTree = foldl (flip add) BinEmpty
+
+addAll :: (Ord a, Foldable t) => t a-> BinTree a -> BinTree a
+addAll = flip $ foldl (flip add)
+
+-- Compute the preorder traversal of a given binary tree
+preorder :: BinTree a -> [a]
+preorder BinEmpty                    = []
+preorder (BinBranch elem left right) = elem : ((preorder left) ++ (preorder right))
+
+-- Compute the inorder traversal of a given binary tree
+inorder :: BinTree a -> [a]
+inorder BinEmpty                    = []
+inorder (BinBranch elem left right) = (inorder left) ++ [elem] ++ (inorder right)
+
+-- Compute the postorder traversal of a given binary tree
+postorder :: BinTree a -> [a]
+postorder BinEmpty                    = []
+postorder (BinBranch elem left right) = (postorder left) ++ (postorder right) ++ [elem]
+
+-- Get the smallest element in a binary tree
+minOf :: BinTree a -> a
+minOf (BinBranch elem BinEmpty _) = elem
+minOf (BinBranch _ left _) = minOf left
+minOf BinEmpty = error "Empty tree lacks a minOf"
+
+-- Get the largest element in a binary tree
+maxOf :: BinTree a -> a
+maxOf (BinBranch elem _ BinEmpty) = elem
+maxOf (BinBranch _ _ right) = maxOf right
+maxOf BinEmpty = error "Empty tree lacks a maxOf"
+
+remove :: (Ord a) => a -> BinTree a -> BinTree a
+remove _ BinEmpty = BinEmpty
+remove item (BinBranch elem left right)
+    | item < elem = BinBranch elem (remove item left) right
+    | item > elem = BinBranch elem left (remove item right)
+    | left == BinEmpty = right
+    | right == BinEmpty = left
+    | otherwise = BinBranch (successor) left (remove successor right)
+        where successor = minOf right
