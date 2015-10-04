391392f30079ee85d561257a1023cffe6de2a495
Allen Zheng <strburst@users.noreply.github.com>
2015-10-03 23:01:17 -0400

    Initial working docdiff function

--- docdiff.hs
+++ docdiff.hs
@@ -1,20 +1,31 @@
 import qualified Data.Map as Map
 
-docdiff :: String -> String -> Integer
-docdiff a b = 0
+-- |Compute the "distance" of one string from another
+docdiff :: (Ord a, Fractional a) => String -> String -> a
+docdiff a b = (dot docVecA docVecB) / (max (mag2 docVecA) (mag2 docVecB))
+    where docVecA = mapFreq a
+          docVecB = mapFreq b
+
+-- |Compute the squared magnitude of a list of numbers
+mag2 :: Num a => [a] -> a
+mag2 a = dot a a
 
 -- |Compute the dot product of two lists of numbers
 dot :: Num a => [a] -> [a] -> a
 dot a b = sum $ zipWith (*) a b
 
--- mapFreq ::
+-- |Replace the words in a string with a list of frequencies corresponding to
+-- each word
+mapFreq :: (Num a) => String -> [a]
+mapFreq string = map (\word -> Map.findWithDefault 0 word counts) $ words string
+    where counts = wordCount string
 
 -- |Count the occurrences of each word in the given string
-wordCount :: String -> Map.Map String Integer
+wordCount :: (Num a) => String -> Map.Map String a
 wordCount = count . words
 
 -- |Count the number of times each item in a list
-count :: (Ord k) => [k] -> Map.Map k Integer
+count :: (Ord k, Num a) => [k] -> Map.Map k a
 count = foldr (incCount) Map.empty
 
 -- |Increment the given key's value
