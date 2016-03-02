ae295bb67c65edaeda54c8fe3a800d5a7f6d8f48
Allen Zheng <strburst@users.noreply.github.com>
2016-03-02 11:02:09 -0500

    Working implementation of localMaxima

--- cis194/hw3/Golf.hs
+++ cis194/hw3/Golf.hs
@@ -7,4 +7,13 @@ everyNth :: Int -> [a] -> [a]
 everyNth n xs =
   if (length xs) < n
     then []
-    else (head $ drop (n - 1) xs) : everyNth n (drop n xs)
+    else (xs !! (n - 1)) : everyNth n (drop n xs)
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = map mid $ filter lmax $ tupGroup3 xs
+  where mid (x, y, z) = y
+        lmax (x, y, z) = x < y && z < y
+
+tupGroup3 :: [a] -> [(a, a, a)]
+tupGroup3 (x:y:z:xs) = (x, y, z) : (tupGroup3 (y:z:xs))
+tupGroup3 _          = []
