2288c3bb72ae21db27598e5b9019ebafe591e32b
Allen Zheng <strburst@users.noreply.github.com>
2016-03-02 22:58:54 -0500

    Working histogram implementation

--- cis194/hw3/Golf.hs
+++ cis194/hw3/Golf.hs
@@ -1,5 +1,7 @@
 module Golf where
 
+import Data.List (intercalate)
+
 skips :: [a] -> [[a]]
 skips base = map ((flip everyNth) base) [1..(length base)]
 
@@ -17,3 +19,31 @@ localMaxima xs = map mid $ filter lmax $ tupGroup3 xs
 tupGroup3 :: [a] -> [(a, a, a)]
 tupGroup3 (x:y:z:xs) = (x, y, z) : (tupGroup3 (y:z:xs))
 tupGroup3 _          = []
+
+histogram :: [Integer] -> String
+histogram xs = intercalate "\n" (lines ++ [eqLine, numLine])
+  where lines = map (\n -> testToStr (>=n) '*' ' ' count) (reverse [1..mode])
+        mode = snd $ foldr1
+          (\a@(_, n1) b@(_, n2) -> if n1 >= n2 then a else b)
+          (tupIndex $ count)
+        count = foldr (changeNth (+1)) (replicate 10 0) xs
+        eqLine = replicate 10 '='
+        numLine = showCat [0..9]
+
+tupIndex :: (Num a) => [t] -> [(a, t)]
+tupIndex = countMap 0
+  where countMap n (x:xs) = (n, x) : countMap (n + 1) xs
+        countMap _ []     = []
+
+changeNth :: (Integral n) => (a -> a) -> n -> [a] -> [a]
+changeNth f 0 (x:xs) = (f x) : xs
+changeNth f n (x:xs) = x : (changeNth f (n - 1) xs)
+changeNth _ _ []     = error "No elements left"
+
+testToStr :: (a -> Bool) -> Char -> Char -> [a] -> [Char]
+testToStr pred ifYes ifNo xs = map decide xs
+  where decide x = if (pred x) then ifYes else ifNo
+
+showCat :: (Show a) => [a] -> [Char]
+showCat xs = foldr cat "" xs
+  where cat x acc = (show x) ++ acc
